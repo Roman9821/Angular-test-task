a1ec6e208a7edd9cbaf55e01b41e8430b1d9c309
Roman9821 <marabyan2021@gmail.com>
2021-10-22 20:20:05 +0400

    Test Task Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, ChangeDetectorRef, Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-task';
+export class AppComponent implements OnInit, AfterViewInit{
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  public defaultCollapse: boolean = false;
+  public renderHtml = false;
+
+  ngOnInit(): void {
+    this.defaultCollapse = true;
+  }
+
+  ngAfterViewInit() : void {
+    this.cdRef.detectChanges();
+  }
+
+  renderHTML() {
+    this.renderHtml = true;
+  }
+
+  public todos:NavigationModel[] = [
+    {
+      term: 't1',
+      termHtml: '<b>T1</b>',
+      count: 1,
+      children: [
+        {
+          term: 't1.1',
+          count: 2,
+          children: [
+            {
+              term: 't1.1.1',
+              count: 3,
+              children: [
+                { term: 't1.1.1.1' },
+                { term: 't1.1.1.2' }
+              ]
+            },
+            {
+              term: 't1.1.2',
+              count: 0
+            }
+          ]
+        }, {
+          term: 't1.2',
+          count: 4
+        }
+      ]
+    }, {
+      term: 2,
+      count: 5,
+      children: [
+        {
+          term: 't2.1',
+          count: 2
+        },
+        {
+          term: 't2.2',
+          children: [
+            { term: 't2.2.1' },
+            { term: 't2.2.2' }
+          ]
+        }
+      ]
+    }
+  ];
+
+}
+
+class NavigationModel {
+  public termHtml?: String;
+  public term?: any;
+  public count?: Number;
+  public children?: NavigationModel[];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule }from '@angular/platform-browser/animations'
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +11,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    FormsModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
